86ecba042af38517c79ca56915f4081b5f15119d
Christian Angermann <christian.angermann@wlw.de>
2018-02-03 14:34:40 +0100

    Setup minimal button properties

--- app/components/essentials/buttons/Button.jsx
+++ app/components/essentials/buttons/Button.jsx
@@ -3,22 +3,62 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
+const propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]),
+  /** Class to add additional styles */
+  className: PropTypes.string,
+  /** If true, disable all interactions for this component */
+  disabled: PropTypes.bool,
+  /** Text to display for blindness accessibility features */
+  title: PropTypes.string,
+  /** The type of the button. [Possible values](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#attr-type) */
+  type: PropTypes.string,
+  /** Handler to be called when the user taps the button */
+  onPress: PropTypes.func
+}
+
+const defaultProps = {
+  type: 'button',
+  className: '',
+  title: '',
+  disabled: false,
+  onPress: () => {}
+}
+
 class Button extends React.Component {
   constructor(props) {
     super(props)
+    this.defaultClassName = 'doc-button'
     this.handleClick = this.handleClick.bind(this)
   }
 
   handleClick(event) {
-    this.props.onClick(event)
+    this.props.onPress(event)
   }
 
   render() {
-    const { children, className, type } = this.props
-    const props = {
-      className: classNames('doc-button', className),
+    const {
+      children,
+      className,
+      disabled,
+      title,
+      type
+    } = this.props
+    let props = {
+      className: classNames(this.defaultClassName, className),
       type,
       onClick: this.handleClick
+    }
+
+    if (title.length) {
+      props = { ...props, title }
+    }
+
+    if (disabled) {
+      props = { ...props, disabled }
     }
 
     return (
@@ -29,20 +69,7 @@ class Button extends React.Component {
   }
 }
 
-Button.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ]),
-  type: PropTypes.string,
-  className: PropTypes.string,
-  onClick: PropTypes.func
-}
-
-Button.defaultProps = {
-  type: 'button',
-  className: '',
-  onClick: () => {}
-}
+Button.propTypes = propTypes
+Button.defaultProps = defaultProps
 
 export default Button
