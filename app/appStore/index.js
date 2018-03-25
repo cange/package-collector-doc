1844f10105fe763ffbdc0c23e5d8397ef362318a
Christian Angermann <psykmedia@gmail.com>
2018-03-25 13:57:02 +0200

    Improve ci build (#6)
    * Fix broken state handling
    * Improve documentation and CI build
    * Reset webpack 4 tasks
    * Fix dev server by downgrading

--- app/appStore/index.js
+++ app/appStore/index.js
@@ -1,12 +1,11 @@
 import { createStore } from 'redux'
 import reducer from './reducer'
-import { toggleNav, updateActiveItem } from './actions'
+import actions from './actions'
 
 export default createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 export {
-  toggleNav,
-  updateActiveItem
+  actions
 }
